e89409b681f443f64ee6264aa42961c21b708b5a
Nandan <nandan.chitale@datastack.tech>
2024-04-12 14:48:28 +0530

    Initial Commit : Added chakra UI, next js app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+
+/**
+ * Represents entire application
+ * Every page and every component is child of this component
+ * @param param0 
+ * @returns 
+ */
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
